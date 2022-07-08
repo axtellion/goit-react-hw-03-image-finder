50917bfe5513cacba254faab07632c3387a6d016
axtellion <slark19735@gmail.com>
2022-07-08 17:47:39 +0300

    start hw_3.2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Box } from './Box';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Box>
+      <GlobalStyle />
+    </Box>
   );
 };
